d48dd69636a34495ee6b65b9e91a72d198a348cd
Chann Becker <chann.becker@zoanmgtinc.com>
2024-03-15 10:34:43 -0700

    Init

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+/*
+**
+**
+**
+*/
+// Initial Includes
+ini_set("display_errors","on");
+
+require_once('Lib/ProgramHeader.php3');
+require_once('Lib/class_Log.php3');
+require_once('Lib/class_fncResult.php3');
+
+ini_set("display_errors","on");
+
+// Start Logging
+$l = new IntranetLog();
+$l->SetEcho(false);
+$l->brk();
+$l->og("{logging title}");
+
+// Required files
+
+// Define local classes
+
+// Define local functions
+
+// Create DB Connections
+
+// Create Objects
+
+// Define Variables
+
+// Script Body
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Channing Becker">
+    <title>Page Title</title>
+    <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/smoothness/jquery-ui.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
+    <script>
+        $(function() {
+            // Handler for .ready() called.
+        });
+    </script>
+
+</head>
+
+<body>
+    Body
+</body>
+
+</html>
